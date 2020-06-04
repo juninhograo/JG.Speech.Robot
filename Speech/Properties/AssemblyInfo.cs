71d0a07399fb93b8d1b9be6d217e6ac74120ebe0
Junior Grão <juninhograo@hotmail.com>
2020-06-04 02:50:08 -0400

    .

--- Speech/Properties/AssemblyInfo.cs
+++ Speech/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Robot")]
+[assembly: AssemblyTitle("JG.Speech")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Robot")]
+[assembly: AssemblyProduct("JG.Speech")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
